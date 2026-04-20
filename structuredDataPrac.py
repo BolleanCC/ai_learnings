d8aa9bf136aabd422c0cf75cda6fc6cb14d62666
BolleanCC <0820songt@gmail.com>
2026-04-20 11:57:24 -0600

    structured data

---
+++ structuredDataPrac.py
@@ -0,0 +1,49 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from anthropic import Anthropic
+
+client = Anthropic()
+model = "claude-haiku-4-5"
+
+
+# helper functions
+def add_user_message(messages, text):
+    user_message = {"role": "user", "content": text}
+    messages.append(user_message)
+
+def add_assistant_message(messages, text):
+    assistant_message = {"role": "assistant", "content": text}
+    messages.append(assistant_message)
+
+# Implementing temperature in code
+def chat(messages, system=None, temperature=1.0, stop_sequences=None):
+    params = {
+        "model": model,
+        "max_tokens": 1000,
+        "messages": messages,
+        "temperature": temperature
+    }
+
+    if system:
+        params["system"] = system
+    if stop_sequences:
+        params["stop_sequences"] = stop_sequences
+    
+    message = client.messages.create(**params)
+    return message.content[0].text
+
+# Exercise!
+
+# Make an initial list of messages
+messages = []
+
+prompt = """Give me 3 AWS CLI commands: list all S3 buckets, describe EC2 instances, and get current AWS account ID. Return commands only."""
+
+add_user_message(messages, prompt)
+add_assistant_message(messages, "aws")
+
+text = chat(messages)
+
+# text = chat(messages)
+print(text)
